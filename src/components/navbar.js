6b90ffaa91e9154f343b197e96e5db066401f655
istvanjuhasz <istvan.juhasz@telcotrend.hu>
2022-10-23 20:13:02 +0200

    change navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,44 +1,89 @@
 import Container from 'react-bootstrap/Container';
+import CustomButton from './CustomButton';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link, useNavigate } from "react-router-dom";
-import {signOut2, toastShow} from '../store/actions'
+import { signOut2, toastShow } from '../store/actions'
 import authService from '../services/authService';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Form from 'react-bootstrap/Form';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
 function NavBar() {
     const navigate = useNavigate()
     const isLogged = useSelector(state => state.user.state);
     const dispatch = useDispatch();
 
-    const logOut= () => {
-            authService.logout();
-            dispatch(signOut2());
-            dispatch(toastShow(` sikeres kijelentkezés!`, 'success'))
+    const logOut = () => {
+        authService.logout();
+        dispatch(signOut2());
+        dispatch(toastShow(` sikeres kijelentkezés!`, 'success'))
     }
     return (
-        <div>
-            <Navbar bg="dark" variant="dark">
-                <Container>
-                    <Navbar.Brand href="#home">Juhi's React app</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link className="me-auto" as={Link} to="/">
-                            Home
-                        </Nav.Link>
+        <>
+            {[false].map((expand) => (
+                <Navbar key={expand} bg="dark" variant="dark" expand={expand} className="mb-3">
+                    <Container>
+                        <Navbar.Brand href="#">Juhi's React app</Navbar.Brand>
+                        <Nav className="me-auto">
+                            <Nav.Link className="me-auto" as={Link} to="/">
+                                Home
+                            </Nav.Link>
+                        </Nav>  
+                        <Nav className="me-auto">  
+                            <Nav.Link className="me-auto" onClick={() =>
+                                isLogged ? logOut() : navigate('/login')}>
+                                {isLogged ? 'Sign Out' : 'Sign In'}
+                            </Nav.Link>
+                        </Nav>
+                        <Nav >
+                            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
 
-                        <Nav.Link className="me-auto" as={Link} to="/other">
-                            Other
-                        </Nav.Link>
-                    </Nav>
-                    <Nav className="justify-content-end">
-                    <Nav.Link className="me-auto" onClick={()=> 
-                        isLogged ? logOut() : navigate('/login')}>
-                          {isLogged ? 'Sign Out' : 'Sign In'}
-                    </Nav.Link>
-                    </Nav>
-                </Container>
-            </Navbar>
-        </div>
+                        </Nav>
+                        <Navbar.Offcanvas
+                            id={`offcanvasNavbar-expand-${expand}`}
+                            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                            placement="end"
+                        >
+                            <Offcanvas.Header closeButton>
+                                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                                    Menü
+                                </Offcanvas.Title>
+                            </Offcanvas.Header>
+                            <Offcanvas.Body>
+                                <Nav className="justify-content-end flex-grow-1 pe-3">
+                                    <Nav.Link href="#action1">Home</Nav.Link>
+                                    <Nav.Link href="#action2">Link</Nav.Link>
+                                    <NavDropdown
+                                        title="Dropdown"
+                                        id={`offcanvasNavbarDropdown-expand-${expand}`}
+                                    >
+                                        <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                                        <NavDropdown.Item href="#action4">
+                                            Another action
+                                        </NavDropdown.Item>
+                                        <NavDropdown.Divider />
+                                        <NavDropdown.Item href="#action5">
+                                            Something else here
+                                        </NavDropdown.Item>
+                                    </NavDropdown>
+                                </Nav>
+                                <Form className="d-flex">
+                                    <Form.Control
+                                        type="search"
+                                        placeholder="Search"
+                                        className="me-2"
+                                        aria-label="Search"
+                                    />
+                                    <CustomButton variant="outline-success">Search</CustomButton>
+                                </Form>
+                            </Offcanvas.Body>
+                        </Navbar.Offcanvas>
+                    </Container>
+                </Navbar>
+            ))}
+        </>
     );
 }
 
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,10 +1,8 @@
 import { Row, Col, Stack } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
 import homePageService from '../services/homePageService';
-import { toastShow} from '../store/actions'
-import CustomButton from '../components/CustomButton'
+import { toastShow } from '../store/actions'
 import { useState, useEffect, } from 'react'
-import authService from '../services/authService';
 
 const Home = () => {
 
@@ -16,45 +14,48 @@ const Home = () => {
     const getTitle = async () => {
         setLoading(true);
         try {
-          const result = await homePageService.getHomePage();
-          setTitle(result.data.HomePage[0].Title.titleDescription)
-          console.log(result.data.HomePage[0].Title.titleDescription
+            const result = await homePageService.getHomePage();
+            setTitle(result.data.HomePage[0].Title.titleDescription)
+            console.log(result.data.HomePage[0].Title.titleDescription
             )
         } catch (err) {
-          setError(err.message || "Unexpected Error!");
+            setError(err.message || "Unexpected Error!");
+            dispatch(toastShow('error i loading home page! : ' + err, 'danger'))
         } finally {
-          setLoading(false);
+            setLoading(false);
         }
-      };
-/*     const handleClick = (event) => {
-        event.preventDefault();
-        const user = {
-            userName: 'Anna',
-            email: 'anna@anna.hu',
-            password: 'Anna1234*',
-            role: 'user'
-        }
-        authService.RegisterForm(user)
-        .then((response) => {
-            console.log(response.data)
-            dispatch(toastShow('Sikeres regisztráció', 'success'))
-          })
-          .catch((err) => {
-            console.log(err)
-            dispatch(toastShow('Sikertelen regisztáció', 'danger'))
-          });
-    } */
+    };
+    /*     const handleClick = (event) => {
+            event.preventDefault();
+            const user = {
+                userName: 'Anna',
+                email: 'anna@anna.hu',
+                password: 'Anna1234*',
+                role: 'user'
+            }
+            authService.RegisterForm(user)
+            .then((response) => {
+                console.log(response.data)
+                dispatch(toastShow('Sikeres regisztráció', 'success'))
+              })
+              .catch((err) => {
+                console.log(err)
+                dispatch(toastShow('Sikertelen regisztáció', 'danger'))
+              });
+        } */
 
     useEffect(() => {
         getTitle();
-      }, []);
+    },[]);
 
 
     return (
         <div>
             <Stack gap={5}>
                 <Row className="justify-content-md-center">
-                {loading ? <p>Home page is loading!</p> :<p>{title}</p>}
+                    <Col md="auto">
+                        {loading ? <p>Home page is loading!</p> : <p>{title}</p>}
+                    </Col>
                 </Row>
 
                 {isLogged ? <h3> secret text</h3> : ''}
